006417de6aed10771b48d2e64064055bcbd4af6e
Reggie Mack <thegraphixninja@gmail.com>
2017-02-16 09:45:20 -0600

    Modules and Services
    Modules and Services

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
-var myApp = angular.module('myApp', []);
+var myApp = angular.module('myApp', ['ngMessages', 'ngResource']);
 
-myApp.controller('mainController', function($scope, $log, $filter) {
+myApp.controller('mainController', function($scope, $log, $filter, $resource) {
     
     $scope.name= 'John';
     $scope.formattedname = $filter('uppercase')($scope.name);
     
     $log.info($scope.name);
     $log.info($scope.formattedname);
+    $log.info($resource)
     
 });
