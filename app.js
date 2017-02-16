be54452ba0ec4eddadc6baed1054449bde14c393
Reggie Mack <thegraphixninja@gmail.com>
2017-02-16 14:24:54 -0600

    Multiple Controllers and Multiple Views
    Multiple Controllers and Multiple Views

--- app.js
+++ app.js
@@ -1,34 +1,13 @@
 var myApp = angular.module('myApp', []);
 
-myApp.controller('mainController', ['$scope', '$filter', '$http' function($scope, $filter, $http) {
+myApp.controller('mainController', ['$scope', function($scope) {
     
-    $scope.handle = '';
+    $scope.name = 'Main';
     
-    $scope.lowercasehandle = function() {
-        return $filter('lowercase')($scope.handle);
-    };
-    
-    $scope.characters = 5;
-    
-    $http.get('/api')
-        .success(function (result) {
-            $scope.rules = result;
-        })
-        .error(function (data, status) {
-            console.log(data);                            
-        });
-                                    
-    $scope.newRule = '';
-    $scope.addRule = function() {
-                                    
-        $http.post('/api', { newRule: $scope.newRule })
-            .success(function (result) {
-                $scope.rules = result;
-                $scope.newRule = '';
-            })
-            .error(function (data, status) {
-               console.log(data);
-            }) ;
-    };
+}]);
 
+myApp.controller('secondController', ['$scope', function($scope) {
+    
+    $scope.name = 'Second';
+    
 }]);
