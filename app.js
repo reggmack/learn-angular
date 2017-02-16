0b09ce5fa51cd5598fb73300d7e3be15cef3f383
Reggie Mack <thegraphixninja@gmail.com>
2017-02-16 13:32:55 -0600

    More Directives
    ng-cloak and ng-click

--- app.js
+++ app.js
@@ -2,20 +2,10 @@ var myApp = angular.module('myApp', ['ngMessages', 'ngResource']);
 
 myApp.controller('mainController', ['$scope', '$filter', '$timeout', function($scope, $filter, $timeout) {
     
-    $scope.handle = '';
-    
-    $scope.lowercasehandle = function() {
-        return $filter('lowercase')($scope.handle);
+    $scope.alertClick = function() {
+        alert("Clicked!")
     };
     
-    $scope.characters = 5;
-    
-    $scope.rules = [
-        { rulename: "Must be 5 characters" },
-        { rulename: "Must not be used elsewhere" },
-        { rulename: "Must be cool" }
-    ];
-    
-    console.log($scope.rules);
+    $scope.name = 'John Doe';
     
 }]); //end App
