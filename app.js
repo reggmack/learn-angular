4de7061aae7c199d5572daf9193abc5753d6a678
Reggie Mack <thegraphixninja@gmail.com>
2017-02-16 12:51:21 -0600

    Watchers and the Digest Loop
    Working with Watchers and the Digest Loop

--- app.js
+++ app.js
@@ -1,12 +1,29 @@
 var myApp = angular.module('myApp', ['ngMessages', 'ngResource']);
 
-myApp.controller('mainController', ['$scope', '$filter', function($scope, $filter) {
+myApp.controller('mainController', ['$scope', '$filter', '$timeout', function($scope, $filter, $timeout) {
     
     $scope.handle = '';
     
     $scope.lowercasehandle = function() {
         return $filter('lowercase')($scope.handle);
     }
+    
+    $scope.$watch('handle', function(newValue, oldValue ) {
+        
+        console.info('Changed!');
+        console.log('Old:' + oldValue);
+        console.log('New:' + newValue);
+        
+    });
+    
+    $timeout(function() {
+        
+        $scope.handle = 'newtwitterhandle';
+        console.log('Scope changed!');
+        
+    }, 3000)
+    
+    
     
 //    $timeout(function() {
 //       $scope.name = 'Everybody'; 
