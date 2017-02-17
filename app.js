9026c71f2d63cd874164ec344984f4a77e99c1bb
Reggie Mack <thegraphixninja@gmail.com>
2017-02-16 18:01:27 -0600

    •	Understanding Compile and Links (Pre-Link)
    Understanding Compile, Pre and Post Links

--- app.js
+++ app.js
@@ -77,6 +77,27 @@ myApp.directive("searchResults", function() {
         scope: {
             personObject: "=",
             formattedAddressFunction: "&"
+        },
+        compile: function(elem, attrs) {
+            
+            console.log('Compiling...');
+            //elem.removeAttr('class');
+            console.log(elem.html());
+                        
+                return {
+                    
+                    post: function(scope, elements, attrs) {
+                        console.log('Post-linking...');
+                        
+                        console.log(scope);
+                        if (scope.personObject.name == 'Jane Doe') {
+                            elements.removeAttr('class');
+                        }
+                        console.log(elements);
+                    }
+
+                }
+            
         }
     }
 });
